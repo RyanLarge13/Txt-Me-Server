92e1b92d96a36b917f171fc617b6e41ba79c9ec6
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-03-06 20:57:03 -0500

    Vonage Platform

--- utils/Twilio.js
+++
@@ -1,2 +0,0 @@
-import twilio from 'twilio';
-const client = twilio('YOUR_ACCOUNT_SID', 'YOUR_AUTH_TOKEN');
---
+++ utils/vonage.js
@@ -0,0 +1,20 @@
+import vonage from "@vonage/server-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+const { Vonage } = vonage;
+const vonageClient = new Vonage({
+ apiKey: process.env.VONAGE_API_KEY,
+ apiSecret: process.env.VONAGE_API_SECRET
+});
+
+const sendVerifyTxt = async (number) => {
+ try {
+ const res = await vonageClient.verify
+ .start({
+  number,
+  brand: "Txt Me"
+ });
+} catch (err) {
+ console.log(err)
+}
+} 
