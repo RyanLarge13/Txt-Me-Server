bb41266e995c3a9778cb024c488d8c7aab84c352
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-04-13 09:30:43 -0400

    Verification

--- routes/verifyRouter.js
+++ routes/verifyRouter.js
@@ -1,13 +1,17 @@
 import express from "express";
 import auth from "../middleware/authenticateToken.js";
 import {
-  verifyPhoneCode,
-  verifyEmailCode,
+ verifyPhoneCode,
+ verifyEmailCode,
+ newPinPhone, 
+ newPinEmail
 } from "../controllers/verifyController.js";
 
 const VerifyRouter = express.Router();
 
 VerifyRouter.post("/phone", auth, verifyPhoneCode);
 VerifyRouter.post("/email", auth, verifyEmailCode);
+VerifyRouter.post("/phone/newpin", newPinePhone);
+VerifyRouter.post("/email/newpin", newPinEmail);
 
 export default VerifyRouter;
