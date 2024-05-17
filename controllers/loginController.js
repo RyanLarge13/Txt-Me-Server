253d6c8114de65ffad30cf480d4e6119d2c21ec0
RyanLarge13 <ryanlarge13@gmail.com>
2024-05-16 22:00:27 -0400

    Fixing Route Name

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,31 +3,31 @@ import Valdtr from "../utils/Validator.js";
 import client from "../utils/client.js";
 
 export const fetchUserData = async (req, res) => {
- const user = req.user;
- if (!user) {
-  ResHdlr.authErr(res, "Please login before using the app");
- }
- 
+  const user = req.user;
+  if (!user) {
+    ResHdlr.authErr(res, "Please login before using the app");
+  }
+  return ResHdlr.sucRes(res, "You have successfully logged in", { user: user });
 };
 
 export const magicPin = async (req, res) => {
- const email = req.body.email;
- if (!email || !Valdtr.valEmail(email)) {
-  return ResHdlr.badReq(res, "Please pass in a valid email");
- }
- try {
-  const clientCon = await client.connect();
-  try {
-  } catch (err) {
-   console.log(err);
-   clientCon.end();
-   return ResHdlr.qryErr(res, err);
+  const email = req.body.email;
+  if (!email || !Valdtr.valEmail(email)) {
+    return ResHdlr.badReq(res, "Please pass in a valid email");
   }
- } catch (err) {
-  console.log(err);
-  client.end();
-  return ResHdlr.conErr(res, err, "Magic pin email");
- }
+  try {
+    const clientCon = await client.connect();
+    try {
+    } catch (err) {
+      console.log(err);
+      clientCon.end();
+      return ResHdlr.qryErr(res, err);
+    }
+  } catch (err) {
+    console.log(err);
+    client.end();
+    return ResHdlr.conErr(res, err, "Magic pin email");
+  }
 };
 
 export const phonePin = (req, res) => {};
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,15 +1,15 @@
 import express from "express";
 import auth from "../middleware/authenticateToken.js";
 import {
- magicPin,
- phonePin,
- fetchUserData
+  magicPin,
+  phonePin,
+  fetchUserData,
 } from "../controllers/loginController.js";
 
 const LoginRouter = express.Router();
 
 LoginRouter.post("/email", magicPin);
 LoginRouter.get("/phone", phonePin);
-LoginRouter.get("/", auth, fetchUserData);
+LoginRouter.get("/user", auth, fetchUserData);
 
 export default LoginRouter;
