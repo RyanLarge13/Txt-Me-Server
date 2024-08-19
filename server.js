74c3ab8162701fcf055ba99abd0d9d4bfdb89a45
RyanLarge13 <ryanlarge13@gmail.com>
2024-08-18 23:04:57 -0400

    Clean Up

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 import express from "express";
 import http from "http";
 import parser from "body-parser";
-//import Redis from "ioredis";
 import SignUpRouter from "./routes/signUpRouter.js";
 import LoginRouter from "./routes/loginRouter.js";
 import VerifyRouter from "./routes/verifyRouter.js";
 import UserRouter from "./routes/userRouter.js";
 import { Server } from "socket.io";
-// import MessageController from "./routes/messageController.js";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
@@ -15,8 +13,6 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 const server = http.createServer(app);
-//const redisClient = new Redis();
-//const redisSubscriber = new Redis();
 
 const clients = new Map();
 
@@ -26,43 +22,11 @@ app.use("/signup", SignUpRouter);
 app.use("/login", LoginRouter);
 app.use("/verify", VerifyRouter);
 app.use("/user", UserRouter);
-//app.use("/message", MessageController);
-
-const addNewClient = (userId, socket) => {
-  clients.set(userId, socket);
-};
-
-const removeClient = (userId) => {
-  clients.delete(userId);
-};
-
-const getClient = (userId) => {
-  clients.get(userId);
-};
-
-/*redisSubscriber.subscribe("user:*", (err, count) => {
-  if (err) {
-    console.log(err);
-  }
-});*/
-
-/*redisSubscriber.on("message", async (channel, message) => {
-  const userId = channel.split(":")[1];
-  const socketId = await getClient(userId);
-  if (socketId) {
-    const socket = wws.clients.get(socketId);
-    if (socket) {
-      socket.send(message);
-    }
-  }
-});*/
 
 const io = new Server(server, {
   cors: {
     origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST"],
-    // allowedHeaders: ["Content-Type"],
-    // credentials: false,
   },
 });
 
@@ -74,14 +38,18 @@ io.on("connection", (socket) => {
   }
   socket.on("text-message", (clientMessage) => {
     console.log(clientMessage);
-    const { sender, recipient, message } = clientMessage;
+    const { sender, recipient, message, time } = clientMessage;
     const clientToSendTo = clients.get(recipient);
     if (clientToSendTo) {
       console.log("client to send to");
       io.to(clientToSendTo).emit("text-message", {
         from: sender,
         message: message,
+        time: time,
       });
+      console.log(`Clients list: ${clients}`);
+    } else {
+      console.log("no client to send to");
     }
   });
   socket.on("disconnect", (client) => {
