a60131b2e41c4363fc3df4c074786ca448590d73
= <ryanhudsonlarge13@gmail.com>
2025-03-14 14:14:32 -0400

    Updating Socket Message

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import LoginRouter from "./routes/loginRouter.js";
 import VerifyRouter from "./routes/verifyRouter.js";
 import UserRouter from "./routes/userRouter.js";
 import { Server } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
@@ -33,25 +34,32 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   const number = socket.handshake.query.number;
   console.log("New Connection");
+
   if (!clients.has(number)) {
     clients.set(number, socket.id);
   }
+
   socket.on("text-message", (clientMessage) => {
     console.log(clientMessage);
-    const { sender, recipient, message, time } = clientMessage;
+
+    if (!clientMessage) {
+      console.log("No message sent from client");
+    }
+
     const clientToSendTo = clients.get(recipient);
+    // Validate the client message
+    // Check for back html, etc...
+    // Check for bad information that should not be there
+
     if (clientToSendTo) {
-      console.log("client to send to");
-      io.to(clientToSendTo).emit("text-message", {
-        from: sender,
-        message: message,
-        time: time,
-      });
-      console.log(`Clients list: ${clients}`);
+      io.to(clientToSendTo).emit("text-message", clientMessage);
+      // Send message to DB
     } else {
+      // Send error back to sender
       console.log("no client to send to");
     }
   });
+
   socket.on("disconnect", (client) => {
     console.log("A client disconnected");
     console.log(client);
